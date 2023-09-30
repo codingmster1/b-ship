f7748c021de41f3b46ba23be67c073c628e14e2a
Jeff <doylejeff98@gmail.com>
2023-09-30 13:03:50 -0400

    webpack bundle html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+    mode: "development",
+    entry: {
+        index: './src/index.js'
     },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+            title: 'Development',
+            template: "./src/index.html",
+
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+
 };
