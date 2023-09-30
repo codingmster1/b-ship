f6ad5a72638a86897df32425a21a496182bddead
Jeff <doylejeff98@gmail.com>
2023-09-30 13:47:12 -0400

    tested index bundle js

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -3,6 +3,7 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-
+console.log("yoooo");
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEscUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9iLXNoaXAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJ5b29vb1wiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
